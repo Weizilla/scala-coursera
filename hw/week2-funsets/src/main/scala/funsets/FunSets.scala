1a5011357bebcd96ce88ebbb170809f88cfa0028
Wei Yang <wei@weizilla.com>
2014-11-08 12:30:38 -0500

    finish 2.1 hw

--- hw/week2-funsets/src/main/scala/funsets/FunSets.scala
+++ hw/week2-funsets/src/main/scala/funsets/FunSets.scala
@@ -1,7 +1,5 @@
 package funsets
 
-import common._
-
 /**
  * 2. Purely Functional Sets.
  */
@@ -20,30 +18,55 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-  def singletonSet(elem: Int): Set = ???
+  def singletonSet(elem: Int): Set = {
+    def singleton(in: Int): Boolean = {
+      elem == in
+    }
+    singleton
+  }
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-  def union(s: Set, t: Set): Set = ???
+  def union(s: Set, t: Set): Set = {
+    def unionF(in: Int): Boolean = {
+      s(in) || t(in)
+    }
+    unionF
+  }
 
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-  def intersect(s: Set, t: Set): Set = ???
+  def intersect(s: Set, t: Set): Set = {
+    def intersectF(in: Int): Boolean = {
+      s(in) && t(in)
+    }
+    intersectF
+  }
 
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: Set, t: Set): Set = ???
+  def diff(s: Set, t: Set): Set = {
+    def diffF(in: Int): Boolean = {
+      s(in) && ! t(in)
+    }
+    diffF
+  }
 
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-  def filter(s: Set, p: Int => Boolean): Set = ???
+  def filter(s: Set, p: Int => Boolean): Set = {
+    def filterF(in: Int): Boolean = {
+      s(in) && p(in)
+    }
+    filterF
+  }
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
--- hw/week2-funsets/src/test/scala/funsets/FunSetSuite.scala
+++ hw/week2-funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -47,30 +47,30 @@ class FunSetSuite extends FunSuite {
     assert(1 + 2 === 3)
   }
 
-  
+
   import FunSets._
 
   test("contains is implemented") {
     assert(contains(x => true, 100))
   }
-  
+
   /**
    * When writing tests, one would often like to re-use certain values for multiple
    * tests. For instance, we would like to create an Int-set and have multiple test
    * about it.
-   * 
+   *
    * Instead of copy-pasting the code for creating the set into every test, we can
    * store it in the test class using a val:
-   * 
+   *
    *   val s1 = singletonSet(1)
-   * 
+   *
    * However, what happens if the method "singletonSet" has a bug and crashes? Then
    * the test methods are not even executed, because creating an instance of the
    * test class fails!
-   * 
+   *
    * Therefore, we put the shared values into a separate trait (traits are like
    * abstract classes), and create an instance inside each test method.
-   * 
+   *
    */
 
   trait TestSets {
@@ -82,31 +82,61 @@ class FunSetSuite extends FunSuite {
   /**
    * This test is currently disabled (by using "ignore") because the method
    * "singletonSet" is not yet implemented and the test would fail.
-   * 
+   *
    * Once you finish your implementation of "singletonSet", exchange the
    * function "ignore" by "test".
    */
-  ignore("singletonSet(1) contains 1") {
-    
-    /**
-     * We create a new instance of the "TestSets" trait, this gives us access
-     * to the values "s1" to "s3". 
-     */
+
+  /**
+   * We create a new instance of the "TestSets" trait, this gives us access
+   * to the values "s1" to "s3".
+   */
+  test ("singleton") {
     new TestSets {
       /**
        * The string argument of "assert" is a message that is printed in case
        * the test fails. This helps identifying which assertion failed.
        */
       assert(contains(s1, 1), "Singleton")
+      assert(!contains(s2, 1), "Singleton")
+      assert(!contains(s3, 1), "Singleton")
     }
   }
 
-  ignore("union contains all elements") {
+  test ("union") {
     new TestSets {
       val s = union(s1, s2)
       assert(contains(s, 1), "Union 1")
       assert(contains(s, 2), "Union 2")
       assert(!contains(s, 3), "Union 3")
+    }
+  }
+
+  test ("intersection") {
+    new TestSets {
+      val s = intersect(s1, s2)
+      assert(!contains(s, 1), "Intersection 1")
+      assert(!contains(s, 2), "Intersection 2")
+      assert(!contains(s, 3), "Intersection 3")
+    }
+  }
+
+  test ("diff") {
+    new TestSets {
+      val d = diff(s1, s2)
+      assert(contains(d, 1), "diff 1")
+      assert(!contains(d, 2), "diff 2")
+      assert(!contains(d, 3), "diff 3")
+    }
+  }
+
+  test ("filter") {
+    new TestSets {
+      val f = filter(s1, (x: Int) => x > 0)
+      val f2 = filter(s1, (x: Int) => x < 0)
+      assert(contains(s1, 1), "singleton")
+      assert(contains(f, 1), "filter > 0")
+      assert(!contains(f2, 1), "filter < 0")
     }
   }
 }
